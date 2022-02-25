25718ecd60e85b7ee7bd8f7dc43f625118109eb4
Luca Heß <65811753+blockiyt@users.noreply.github.com>
2022-02-25 20:54:17 +0100

    updated version

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cache = new NodeCache( { useClones: false, maxKeys: 2, deleteOnExpire: tru
 
 //settings
 const port = 3000
-const version = "2.3.4"
+const version = "2.3.5"
 
 //cache flusher
 cron.schedule('0 0 * * *', () => {
